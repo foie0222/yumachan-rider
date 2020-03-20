1521e161fe3a2af3c1395cd3ccb0b3cfaa140cc4
foie0222 <daikinoue0222@gmail.com>
2020-03-21 03:08:59 +0900

    add timestamp

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from scraper import Scraper
 from entry import get_entry
 from ticket import make_ticket
 from writer import make_csv
+import sys
 
 # データを取得する対象
 url = "https://www.ai-yuma.com/?page=1584683595"
 
 
 def main():
+    timestamp = sys.argv[1]
 
     scraper = Scraper(url)
     header_txt = scraper.get_get_header_txt()
@@ -16,7 +18,7 @@ def main():
 
     ticket_list = make_ticket(entry)
 
-    make_csv(ticket_list)
+    make_csv(ticket_list, timestamp)
 
 
 if __name__ == '__main__':
--- writer.py
+++ writer.py
@@ -1,4 +1,4 @@
-def make_csv(ticket_list):
-    with open('./ticket.csv', 'w') as f:
+def make_csv(ticket_list, timestamp):
+    with open('./ticket_' + timestamp + '.csv', 'w') as f:
         for ticket in ticket_list:
             f.write(ticket.to_csv() + '\n')
