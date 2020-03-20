e6b503507ebab32598afcefafb24a0b4707b4c0f
foie0222 <daikinoue0222@gmail.com>
2020-03-21 02:59:38 +0900

    modified for file format

--- writer.py
+++ writer.py
@@ -1,8 +1,4 @@
-import csv
-
-
 def make_csv(ticket_list):
     with open('./ticket.csv', 'w') as f:
-        writer = csv.writer(f)
         for ticket in ticket_list:
-            writer.writerow([ticket.to_csv()])
+            f.write(ticket.to_csv() + '\n')
