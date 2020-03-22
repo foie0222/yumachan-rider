d1f651f23a19813211a761630be40b263750cf7a
foie0222 <daikinoue0222@gmail.com>
2020-03-22 11:22:26 +0900

    make better bet logic during races

--- ticket.py
+++ ticket.py
@@ -23,20 +23,19 @@ def make_ticket(entry, realtime_odds_list):
     axis_list = [entry.horse_list[0]]  # 1番高確率馬は軸馬に入れておく
     braid_list = []  # 紐馬には何も入れない
 
-    # 軸馬（上位5頭のうち軸のサインがある、もしくは単勝回収率110%以上）の単勝を購入
-    for horse in entry.horse_list[:4]:
+    # 軸馬（上位6頭のうち軸のサインがある、もしくは単勝回収率110%以上）の単勝を購入
+    for horse in entry.horse_list[:5]:
         bet = 0
         odds = list(filter(lambda realtime_odds: True if realtime_odds.umano == horse.umano else False, realtime_odds_list))[0]
-        print(type(odds))
 
         if horse.sign == 'axis':
-            bet += 500  # 軸馬だったら500円ベット
+            bet += 300  # 軸馬だったら300円ベット
         if horse.probability * odds.tanodds >= 110:
-            bet += 100  # 単勝回収率が110%以上なら追加で100円ベット
+            bet += 100  # 単勝回収率が110%以上なら追加で300円ベット
         if horse.probability * odds.tanodds >= 120:
             bet += 200  # 単勝回収率が120%以上なら追加で200円ベット
         if horse.probability * odds.tanodds >= 130:
-            bet += 300  # 単勝回収率が120%以上なら追加で200円ベット
+            bet += 300  # 単勝回収率が130%以上なら追加で200円ベット
 
         if bet == 0:  # betが0なら次へ
             continue
@@ -63,6 +62,8 @@ def make_ticket(entry, realtime_odds_list):
     # 紐馬と軸馬とのワイドを購入
     for braid in braid_list:
         for axis in axis_list:
+            if axis.umano == braid.umano:
+                continue
             ticket = Ticket(entry.opdt, entry.rcourcecd, entry.rno, 'WIDE', 'NORMAL', '', make_wide(axis.umano, braid.umano),
                             '200')
             ticlet_list.append(ticket)
