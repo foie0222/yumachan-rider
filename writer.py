d63c89b77755cd55dd035753edbf0ae8bf268441
foie0222 <daikinoue0222@gmail.com>
2020-03-23 19:49:50 +0900

    make tickets dir

--- writer.py
+++ writer.py
@@ -1,4 +1,14 @@
+import os
+
 def make_csv(ticket_list, timestamp):
-    with open('./ticket_' + timestamp + '.csv', 'w') as f:
+    make_tickets_dir()
+    with open('./tickets/ticket_' + timestamp + '.csv', 'w') as f:
         for ticket in ticket_list:
             f.write(ticket.to_csv() + '\n')
+
+# ticketsフォルダがなかったら作成する
+def make_tickets_dir():
+    if not os.path.isdir('./tickets'):
+        os.mkdir('./tickets')
+    else:
+        pass
