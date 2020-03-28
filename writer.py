b9d9c20143524d4955a46fdb461719fd6f82d891
foie0222 <daikinoue0222@gmail.com>
2020-03-29 02:28:05 +0900

    refactoring

--- writer.py
+++ writer.py
@@ -1,10 +1,12 @@
 import os
 
+
 def make_csv(ticket_list, timestamp):
     make_tickets_dir()
     with open('./tickets/ticket_' + timestamp + '.csv', 'w') as f:
         for ticket in ticket_list:
             f.write(ticket.to_csv() + '\n')
+
 
 # ticketsフォルダがなかったら作成する
 def make_tickets_dir():
