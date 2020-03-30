dadc625ea6f6d2f3ae155a8f140c9cdee436d64f
foie0222 <daikinoue0222@gmail.com>
2020-03-31 01:58:04 +0900

    make sheet logic

--- gss.py
+++ gss.py
@@ -11,12 +11,28 @@ def write_gss(ticket_list, timestamp):
     credentials = ServiceAccountCredentials.from_json_keyfile_name('credential/keibaautovote.json', scope)
     gc = gspread.authorize(credentials)
 
-    # シート名は先頭から8桁
-    sheet_name = timestamp[:8]
-    worksheet = gc.open_by_key(SPREADSHEET_KEY).worksheet(sheet_name)
+    # 対象のスプレッドシートを取得
+    spreadsheet = gc.open_by_key(SPREADSHEET_KEY)
 
+    # ワークシート一覧にシートがあるかチェックしてなければ作成する
+    worksheets = spreadsheet.worksheets()
+    sheet_name = timestamp[:8]
+    if not existSheet(worksheets, sheet_name):
+        spreadsheet.add_worksheet(title=sheet_name, rows=150, cols=26)
+    
+    # 対象のシートを指定
+    worksheet = spreadsheet.worksheet(sheet_name)
+    
     # 購入リストを1件ずつスプレッドシートに書き出す
     for ticket in ticket_list:
         rowToAdd = [ticket.opdt, ticket.rcourcecd, ticket.rno, ticket.denomination, ticket.method, ticket.number,
                     ticket.bet_price]
         worksheet.append_row(rowToAdd)
+
+
+# シートの存在チェック
+def existSheet(worksheets, sheet_name):
+    for sheet in worksheets:
+        if sheet_name == sheet.title:
+            return True
+    return False
