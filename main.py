98dda02e9bd2572995c4c8a51b5d98cde7214113
foie0222 <daikinoue0222@gmail.com>
2020-04-02 02:45:16 +0900

    add verification logic

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from gss import write_gss
 from odds import get_realtime_odds
-from scraper import Scraper
+from scraper import Scraper, get_date, get_url, isLocal
 from entry import get_entry
 from ticket import make_ticket
-from writer import make_csv
+from writer import make_csv, write_races_csv
 import sys
+import time
 
 # データを取得する対象
 URL = "https://www.ai-yuma.com/"
@@ -15,8 +16,8 @@ def main():
 
     # ゆまちゃんのHRから情報を取得
     scraper = Scraper(URL)
-    header_txt = scraper.get_get_header_txt()
-    body_txt = scraper.get_get_body_txt()
+    header_txt = scraper.get_header_txt()
+    body_txt = scraper.get_body_txt()
 
     # 取得した結果を変換
     entry = get_entry(header_txt, body_txt)
@@ -32,7 +33,69 @@ def main():
 
     # 購入馬券リストをcsvに書き出す
     make_csv(ticket_list, timestamp)
+    
+    time.sleep(3)
+
+
+
+def verify(opdt):
+    with open('races/' + opdt + '.txt') as lines:
+        for line in lines:
+            URL = line.rstrip()
+
+            # ゆまちゃんのHRから情報を取得
+            scraper = Scraper(URL)
+            header_txt = scraper.get_header_txt()
+            body_txt = scraper.get_body_txt()
+
+            # 「本日の結果」だったらスキップ
+            if '結果' in header_txt:
+                continue
+
+            # 取得した結果を変換
+            entry = get_entry(header_txt, body_txt)
+
+            # リアルタイムオッズを取得
+            realtime_odds = get_realtime_odds(entry.opdt, entry.rcourcecd, entry.rno)
+
+            # 購入馬券リストを作る
+            ticket_list = make_ticket(entry, realtime_odds)
+
+            # 購入馬券リストをGSSに書き出す
+            write_gss(ticket_list, opdt)
+
+            # # 購入馬券リストをcsvに書き出す
+            # make_csv(ticket_list, opdt)
+            
+            # 次のループまで3秒待つ
+            time.sleep(3)
+
+
+# 検証のためのURLを取得するためのロジック
+def url_scrape(loop):
+    url = URL
+    count = 0
+    while count < loop:
+
+        scraper = Scraper(url)
+        entry_inner_list = scraper.get_entry_inner_list()
+        
+        for entry_inner in entry_inner_list:
+            if isLocal(entry_inner): # 地方競馬ならスキップ
+                continue
+            date = get_date(entry_inner) # like 20200330
+            url = get_url(entry_inner) # レースのURL
+            write_races_csv(date, url) # csv書き出し
+            time.sleep(3) # 次のループまで3秒待つ
+        
+        url = scraper.get_next_url()
+        count += 1
 
 
 if __name__ == '__main__':
-    main()
+    if sys.argv[1] == 'verification':
+        verify(sys.argv[2]) # 検証したい日付
+    elif sys.argv[1] == 'scraping':
+        url_scrape(int(sys.argv[2])) # ループを回したい回数
+    else:
+        main()
