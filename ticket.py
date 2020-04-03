55116f1f3887c55d39f8337d2a8dcde871d7455a
foie0222 <daikinoue0222@gmail.com>
2020-04-04 08:54:07 +0900

    make wide vote possibility

--- ticket.py
+++ ticket.py
@@ -36,15 +36,15 @@ def make_ticket(entry, realtime_odds):
         if horse.probability * odds.tanodds >= 130:  # 単勝回収率が130%以上なら払い戻しが3000円超える最低金額をベット
             bet = lowest_bet_for(3000, odds.tanodds)
 
-        if bet > 0 or horse.sign == 'axis' or index <= 1:  # 購入金額が100円以上もしくは軸の表記がある馬、最も高確率の馬は軸馬リストに追加
+        if horse.probability * odds.tanodds > 90 or horse.sign == 'axis' or index <= 1:  # 購入金額が100円以上もしくは軸の表記がある馬、最も高確率の馬は軸馬リストに追加
             axis_list.append(horse)
 
-        if odds.tanodds < 3 or bet == 0:  # 単勝オッズが3倍以下なら購入見送り
-            continue
+        # if odds.tanodds < 3 or bet == 0:  # 単勝オッズが3倍以下なら購入見送り
+        #     continue  
         ticket = Ticket(entry.opdt, entry.rcourcecd, entry.rno, 'TANSYO', 'NORMAL', '', horse.umano, str(bet))
         ticlet_list.append(ticket)
 
-    # 紐馬（紐のサインがある、もしくは単勝回収率170%以上）の単勝・複勝を購入
+    # 紐馬（紐のサインがある、もしくは単勝回収率170%以上）の複勝を購入
     for horse in entry.horse_list:
 
         odds = list(filter(lambda real_odds: True if real_odds.umano == horse.umano else False, realtime_tan_odds_list))[0]
@@ -53,12 +53,12 @@ def make_ticket(entry, realtime_odds):
         if horse.probability * odds.tanodds >= 170:  # 単勝回収率が170%以上なら払い戻しが2000円超える最低金額をベット
             bet = lowest_bet_for(2000, odds.tanodds)
 
-        if bet > 0 or horse.sign == 'braid':  # 購入金額が100円以上か紐の表記がある馬は紐馬リストに追加
+        if horse.probability * odds.tanodds >= 130 or horse.sign == 'braid':  # 購入金額が100円以上か紐の表記がある馬は紐馬リストに追加
             braid_list.append(horse)
 
         if bet == 0 or is_in_list(horse.umano, axis_list):  # 軸馬なら購入見送り
             continue
-        ticket_tan = Ticket(entry.opdt, entry.rcourcecd, entry.rno, 'TANSYO', 'NORMAL', '', horse.umano, str(bet))
+        ticket_tan = Ticket(entry.opdt, entry.rcourcecd, entry.rno, 'FUKUSYO', 'NORMAL', '', horse.umano, str(bet))
         ticlet_list.append(ticket_tan)
 
     realtime_wide_odds_list = realtime_odds.wide_odds_list
@@ -71,7 +71,7 @@ def make_ticket(entry, realtime_odds):
             pair_num = make_wide(braid.umano, axis.umano)
             odds = list(filter(lambda real_odds: True if pair_num == real_odds.pair_umano else False, realtime_wide_odds_list))[0]
 
-            if odds.wideodds < 10:
+            if odds.wideodds * braid.probability * axis.probability * 4 < 100:
                 continue
             
             bet = lowest_bet_for(2000, odds.wideodds)
