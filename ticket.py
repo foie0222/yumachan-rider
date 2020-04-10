e9e94f21c7de0ead92a6accae3a08925b443c692
foie0222 <daikinoue0222@gmail.com>
2020-04-11 02:45:30 +0900

    modified bet logic

--- ticket.py
+++ ticket.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Ticket:
     def __init__(
             self,
@@ -48,62 +51,69 @@ def make_ticket(entry, realtime_odds):
     axis_list = []
     braid_list = []
 
+    # 単勝購入
     realtime_tan_odds_list = realtime_odds.tan_odds_list
-
-    # 軸馬の単勝馬券を購入
-    for index, horse in enumerate(entry.horse_list[:5]):
+    for horse in entry.horse_list[:5]:
+        # 軸馬一覧に追加
+        axis_list.append(horse)
 
         odds = list(filter(lambda real_odds: True if real_odds.umano ==
                            horse.umano else False, realtime_tan_odds_list))[0]
 
         bet = 0
-        if horse.probability * odds.tanodds >= 120:  # 単勝回収率が120%以上なら払い戻しが2000円超える最低金額をベット
-            bet = lowest_bet_for(2000, odds.tanodds)
-        if horse.probability * odds.tanodds >= 130:  # 単勝回収率が130%以上なら払い戻しが3000円超える最低金額をベット
-            bet = lowest_bet_for(3500, odds.tanodds)
+        expected_value = horse.probability * odds.tanodds
+        if expected_value >= 120:  # 単勝回収率が120%以上なら期待値に応じてベット
+            bet = lowest_bet_for(expected_value * 15, odds.tanodds)
 
-        if horse.probability > 10 or horse.sign == 'axis':  # 購入金額が100円以上もしくは軸の表記がある馬、最も高確率の馬は軸馬リストに追加
-            axis_list.append(horse)
-
-        if bet == 0:  # 単勝オッズが3倍以下なら購入見送り
+        if bet == 0:
             continue
         ticket = Ticket(entry.opdt, entry.rcoursecd, entry.rno,
                         'TANSYO', 'NORMAL', '', horse.umano, str(bet))
         ticlet_list.append(ticket)
 
-    # 紐馬（紐のサインがある、もしくは単勝回収率170%以上）の複勝を購入
+    # 複勝購入
+    fuku_min_odds_list = realtime_odds.fuku_min_odds_list
     for horse in entry.horse_list:
 
         odds = list(filter(lambda real_odds: True if real_odds.umano ==
-                           horse.umano else False, realtime_tan_odds_list))[0]
+                           horse.umano else False, fuku_min_odds_list))[0]
+
+        fuku_probability = get_fuku_probability(horse.probability)
 
         bet = 0
-        if horse.probability * odds.tanodds >= 170:  # 単勝回収率が170%以上なら払い戻しが2000円超える最低金額をベット
-            bet = lowest_bet_for(3000, odds.tanodds)
+        if fuku_probability * odds.fuku_min_odds >= 120:  # 複勝回収率が120%以上なら払い戻しが3500円超える最低金額をベット
+            bet = lowest_bet_for(3500, odds.fuku_min_odds)
 
-        if bet >= 100 or horse.sign == 'braid':  # 購入金額が100円以上か紐の表記がある馬は紐馬リストに追加
-            braid_list.append(horse)
+        if bet >= 100 or horse.sign == 'braid':
+            bet += 100
 
-        if bet == 0 or is_in_list(horse.umano, axis_list):  # 軸馬なら購入見送り
+        if bet == 0:
             continue
+
         ticket_fuku = Ticket(entry.opdt, entry.rcoursecd, entry.rno,
                              'FUKUSYO', 'NORMAL', '', horse.umano, str(bet))
         ticlet_list.append(ticket_fuku)
+        braid_list.append(horse)
 
     realtime_wide_odds_list = realtime_odds.wide_odds_list
 
-    # 紐馬と軸馬とのワイドを購入
-    for braid in braid_list:
-        for axis in axis_list:
+    # ワイドを購入
+    for axis in axis_list:
+        for braid in braid_list:
             if axis.umano == braid.umano:
                 continue
-            pair_num = make_wide(braid.umano, axis.umano)
+
+            pair_num = make_wide(axis.umano, braid.umano)
             odds = list(
                 filter(
                     lambda real_odds: True if pair_num == real_odds.pair_umano else False,
                     realtime_wide_odds_list))[0]
 
-            if odds.wideodds * braid.probability * axis.probability * 4 * 1.2 / 100 < 120:
+            axis_in_wide_probability = get_fuku_probability(axis.probability)
+            braid_in_wide_probability = get_fuku_probability(braid.probability)
+
+            if odds.wideodds * axis_in_wide_probability * \
+                    braid_in_wide_probability / 100 < 300:
                 continue
 
             bet = lowest_bet_for(3000, odds.wideodds)
@@ -155,3 +165,14 @@ def get_axis_list(horse_list):
             axis_list.append(horse)
 
     return axis_list
+
+
+# 単勝率から複勝率を算出
+def get_fuku_probability(tan_probability):
+    res = -0.00001 * tan_probability ** 4 + 0.0021 * tan_probability ** 3 - \
+        0.1344 * tan_probability ** 2 + 4.4261 * tan_probability + 0.8811
+    return res if res < 100 else 100
+
+
+if __name__ == '__main__':
+    print(get_fuku_probability(1.7))
