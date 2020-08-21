daf249753a487a3f859f9d30dc3e3ddc2ecf2b7d
foie0222 <daikinoue0222@gmail.com>
2020-08-22 00:34:14 +0900

    modified rate

--- ticket.py
+++ ticket.py
@@ -73,7 +73,7 @@ def make_ticket(entry, realtime_odds):
         bet = 0
         expected_value = horse.probability * odds.tanodds
         if expected_value >= 180 and odds.tanodds <= 300 and odds.tanodds >= 3.0:
-            bet = lowest_bet_for(expected_value * 70, odds.tanodds)
+            bet = lowest_bet_for(expected_value * 150, odds.tanodds)
         else:
             continue
 
@@ -106,7 +106,7 @@ def make_ticket(entry, realtime_odds):
         bet = 0
         expected_value = fuku_probability * odds.fuku_min_odds
         if expected_value >= 120 and odds.fuku_min_odds <= 7.0 and odds.fuku_min_odds >= 1.5:
-            bet = lowest_bet_for(expected_value * 80, odds.fuku_min_odds)
+            bet = lowest_bet_for(expected_value * 200, odds.fuku_min_odds)
         else:
             continue
 
@@ -147,7 +147,7 @@ def make_ticket(entry, realtime_odds):
 
             bet = 0
             if expected_value >= 300 and odds.wideodds <= 150 and odds.wideodds >= 20:
-                bet = lowest_bet_for(expected_value * 60, odds.wideodds)
+                bet = lowest_bet_for(expected_value * 80, odds.wideodds)
             else:
                 continue
 
